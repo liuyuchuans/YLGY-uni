14aebe9220bfd06563c90e8dd03806f59939702f
liuyuchuan <lyc_web@163.com>
2024-07-01 14:50:06 +0800

    修改登录的一些信息 和卡片显示的一些问题

--- yulegeyu-master/src/pages/LoginPage.vue
+++ yulegeyu-master/src/pages/LoginPage.vue
@@ -12,17 +12,17 @@
         <a-form-item
         label="账号"
         name="username"
-        :rules="[{ required: true, message: 'Please input your username!' }]"
+        :rules="[{ required: true, message: '请输入账号！' }]"
         >
-        <a-input v-model:value="formState.username" />
+        <a-input placeholder="请输入账号/手机号" v-model:value="formState.username" />
         </a-form-item>
 
         <a-form-item
         label="密码"
         name="password"
-        :rules="[{ required: true, message: 'Please input your password!' }]"
+        :rules="[{ required: true, message: '请输入密码！' }]"
         >
-        <a-input-password v-model:value="formState.password" />
+        <a-input-password placeholder="请输入密码" v-model:value="formState.password" />
         </a-form-item>
 
         <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
